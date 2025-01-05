f28721266fd95de79ee76e061beeeb790753f69b
Zetka <jakub.kochanski10@gmail.com>
2025-01-05 11:50:39 +0100

    migration

---
+++ API/Data/migrations/20241207135308_addedDescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.migrations
+{
+    /// <inheritdoc />
+    public partial class addedDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "FitnessPlanTemplates",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "FitnessPlanTemplates");
+        }
+    }
+}
